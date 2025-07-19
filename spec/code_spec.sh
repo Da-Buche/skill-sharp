63603cd48d0bb06c6580caa5547c8868c42a6fc7
Aurélien Buchet <aurel_dev@dabuche.com>
2025-07-19 21:20:08 +0200

    Waived intentional word splitting shellspec checks

--- spec/code_spec.sh
+++ spec/code_spec.sh
@@ -16,6 +16,7 @@ export -f list_files
 It 'contains no "TODO"'
 Skip if "fd not available" missing_fd
 Pending "TODOs are not failures but they should be reminded."
+# shellcheck disable=SC2046 # Intended word splitting
 When run grep -Eni 'TO-?DO' $(list_files --exclude='code_spec.sh')
 The stdout should be blank
 The stderr should be blank
@@ -25,6 +26,7 @@ End
 
 It 'contains no "DEBUG"'
 Skip if "fd not available" missing_fd
+# shellcheck disable=SC2016 # Intended quoted variable
 When run env FILES="$(list_files --exclude='code_spec.sh' --exclude='Makefile' --exclude='README.md')" bash -c 'grep -Eni "\bDEBUG\b" $FILES | sed "s/@debug//g" | grep -Ei "\bDEBUG\b"'
 The stdout should be blank
 The stderr should be blank
@@ -34,6 +36,7 @@ End
 
 It 'does not contain whitespace'
 Skip if "fd not available" missing_fd
+# shellcheck disable=SC2046 # Intended word splitting
 When run grep -En '\s+$' $(list_files --exclude='README.md')
 The stdout should be blank
 The stderr should be blank
@@ -43,6 +46,7 @@ End
 
 It 'does not contain tabs'
 Skip if "fd not available" missing_fd
+# shellcheck disable=SC2046 # Intended word splitting
 When run grep -n $'\t' $(list_files --exclude='Makefile')
 The stdout should be blank
 The stderr should be blank
@@ -52,6 +56,7 @@ End
 
 It 'does not contain carriage returns'
 Skip if "fd not available" missing_fd
+# shellcheck disable=SC2046 # Intended word splitting
 When run grep -n $'\r' $(list_files)
 The stdout should be blank
 The stderr should be blank
@@ -71,7 +76,8 @@ End
 It 'contains 15-25% of comments'
 Skip if "scc not available" missing_scc
 comments_ratio() { scc | grep -E ^Total | awk '{ printf "%.2f\n", ($5 * 100 / ($5 + $6)) }'; }
-When run env COMMENTS_RATIO=$(comments_ratio) bash -c 'echo "15.00 <= $COMMENTS_RATIO && $COMMENTS_RATIO <= 25.00" | bc -l'
+# shellcheck disable=SC2016 # Intended quoted variable
+When run env COMMENTS_RATIO="$(comments_ratio)" bash -c 'echo "15.00 <= $COMMENTS_RATIO && $COMMENTS_RATIO <= 25.00" | bc -l'
 The stdout should equal 1
 The stderr should be blank
 The status should be success
