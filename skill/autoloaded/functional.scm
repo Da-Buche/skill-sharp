d992c86a8c5ea4e0a263718673a587dbc97ef379
Aurélien Buchet <aurel_dev@dabuche.com>
2025-08-20 11:42:22 +0200

    Added `@queue' and `@timer' functions.

--- skill/autoloaded/functional.scm
+++ skill/autoloaded/functional.scm
@@ -32,5 +32,72 @@
   (lambda (obj) (apply '@get obj props))
   )
 
+;; -------------------------------------------------------
+;; `hiEnqueueCmd' wrapper
+;; -------------------------------------------------------
+
+(let ( ( queue (tconc nil nil) )
+       )
+
+  (@fun _\@queue ()
+    ?doc "`@queue' helper to be called inside `hiEnqueueCmd'."
+    ?out t
+    ?global t
+    (while (cdar queue)
+      (@if (cddar queue) (funcall (popf (cdar queue)))
+        ;; Prevent queue from becoming empty and breaking tconc structure
+        (tconc queue nil)
+        (assert (not (popf (car queue))) "_\\@queue - A non-nil object was removed from queue...")
+        (funcall (popf (car queue)))
+        ))
+    ;; Always return t
+    t)
+
+  (@fun @queue
+    ( ( function ?type callable )
+      )
+    ?doc "Call function at the end of the event queue. This is `hiEnqueueCmd' functional wrapper."
+    ?out t
+    ?global t
+    (tconc queue function)
+    (hiEnqueueCmd "(_\\@queue)")
+    )
+
+  )
+
+;; -------------------------------------------------------
+;; `hiRegTimer' wrapper
+;; -------------------------------------------------------
+
+(let ( ( queue_dpl (list nil) )
+       ( i         0          )
+       )
+
+  (@fun _\@timer
+    ( ( id ?type integer )
+      )
+    ?doc "`@timer' helper to be called inside `hiRegTimer'"
+    ?out t
+    ?global t
+    (funcall (car (remprop queue_dpl id)))
+    ;; Always return t
+    t)
+
+  (@fun @timer
+    ( ( tenths_of_second ?type integer  )
+      ( function         ?type callable )
+      )
+    ?doc "Execute FUNCTION after TENTHS_OF_SECOND. This is `hiRegTimer' functional wrapper."
+    ?out t
+    ?global t
+    ;; `remprop' works with integer but `putprop' does not
+    ;; adding dpl prop by hand
+    (pushf function (cdr queue_dpl))
+    (pushf ++i      (cdr queue_dpl))
+    (hiRegTimer (lsprintf "(_\\@timer %d)" i) tenths_of_second)
+    )
+
+  );let
+
 ;*/
 
