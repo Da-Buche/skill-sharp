3395ab0fd850c63a9de4c9b0580fdd25a389c939
Aurélien Buchet <aurel_dev@dabuche.com>
2025-09-17 11:23:32 +0200

    add possibility to pretty-print lists in @to_string and @str using @str.pretty switch

--- skill/macros/f-strings.scm
+++ skill/macros/f-strings.scm
@@ -10,15 +10,17 @@
     )
   ?doc "Return OBJ as a string (like `printself').
 If OBJ is already a string, it is returned as is.
+If @str.pretty is non-nil and OBJ is a list: it is pretty-printed.
 
 This function is meant to be used as `@str' macro helper to avoid double-quotes when inserting strings.
 Those double-quotes are easily added by hand when required while removing afterwards is way more complicated.
 This design choice is also consistent with Python's f-strings."
   ?out string
-  (if (stringp obj)
-      obj
-    (lsprintf spec obj)
-    ));if ;fun
+  (cond
+    ( (stringp obj)                  obj                 )
+    ( (and (listp obj) @str.pretty ) (@pretty_print obj) )
+    ( t                              (lsprintf spec obj) )
+    ));cond ;fun
 
 (let ( in out args char translate error_message
        ;; TODO - Those are defined only because Lint does not suppport @fun properly yet
@@ -144,6 +146,8 @@ Everything between '{' and '}' will be evaluated in current environment.
 
 Expressions inside '{' and '}' can end with any format specification like %A or %-12.27f.
 (As described in `fprintf' documentation.)
+
+If @str.pretty is non-nil, lists are pretty-printed.
 "
   ;; No idea why the type letter in arguments template is not taken in account
   ;; Without the following line, when running (@str symbol)
