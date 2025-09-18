5520b5989a45eefec8bcb1091b4b97ba648ba946
Aurélien Buchet <aurel_dev@dabuche.com>
2025-09-18 08:34:05 +0200

    lint now supports environments and detect unused variables in `let`

--- skill/autoloaded/lint.scm
+++ skill/autoloaded/lint.scm
@@ -4,27 +4,56 @@
 ;; A. Buchet - September 2025
 ;; ===============================================================================================================
 
+
+(println 12)
+
 ;; DEBUG
 ;(@set_debug t)
+
+;; =======================================================
+;; Fixes arguments check for macros and syntax forms
+;; =======================================================
+
+;; TODO - Contact Cadence support about (arglist 'defun) and (arglist 'if)
+
+;; No idea why but (arglist 'if) mentions @rest for else part
+(setf (@arglist 'if)
+  '( ( g_general  ?type general          )
+     ( g_general  ?type general          )
+     ( g_general  ?type general ?def nil )
+     ))
+
+;; No idea why but (arglist 'defun) does not mention body
+(setf (@arglist 'defun)
+  '( ( s_symbol  ?type symbol  )
+     ( g_general ?type general )
+     ( g_general ?type general )
+     @rest
+     ( body ?type ( general ... ) )
+     ))
+
+(setf (@arglist 'defglobalfun) (@arglist 'defun))
 
 ;; =======================================================
 ;; Add Rule
 ;; =======================================================
 
 (@fun @lint_default_rule
-  ( ( sexp     ?type list  )
-    ( messages ?type tconc )
-    ( levels   ?type list  )
-    ( parents  ?type list  )
+  ( ( sexp     ?type list    )
+    ( messages ?type tconc   )
+    ( levels   ?type list    )
+    ( parents  ?type list    )
+    ( envs     ?type list    )
+    ( scheme   ?type boolean )
     )
   ?doc "Apply Lint recursively on all sublists of SEXP.
 This is the default Lint 'control' rule."
   ?out nil
   ;(@debug "@lint_default_rule - sexp: {sexp}\n  messages: {messages}\n\n")
-  (let ( ( sexp_pos 0)
+  (let ( ( sexp_pos 0 )
          )
     (foreach sub_sexp sexp
-      (_\@lint_sexp sub_sexp messages (cons sexp_pos++ levels) (cons sexp parents))
+      (_\@lint_sexp sub_sexp messages (cons sexp_pos++ levels) (cons sexp parents) envs scheme)
       ))
   nil)
 
@@ -49,21 +78,29 @@ This is the default Lint 'control' rule."
     t
     );fun
 
-  (@fun _\@lint_get_rules
-    ( ( fun ?type symbol ) )
+  (@fun lint_get_rules
+    ( ( fun ?type symbol )
+      )
     ?doc "Return rules associated to FUN."
     ?out ( callable ... )
     (cons control_rule_by_fun[fun] rules_by_fun[fun])
     )
 
-  (defglobalfun _\@lint_sexp ( sexp messages levels parents )
+  (defglobalfun _\@lint_sexp ( sexp messages levels parents envs scheme )
     "Lint SEXP."
     ;(@debug "_@lint_sexp\n  sexp: {sexp}\n  messages: {messages}\n\n")
     (cond
       ;; Symbol, check variable
       ( (symbolp sexp)
-        ;; TODO - Make sure variable exists, mark it as used
-        
+        ;; Make sure variable exists, mark it as used
+        (let ( ( env (car (exists env envs env[sexp])) )
+               )
+          (if env
+              (setf env[sexp]->status 'used)
+            ;; TODO - Check if global variable exists (report it)
+            ;; TODO - Report if variable is undefined
+            ()
+            ))
         )
       ;; Atom, skip it
       ( (atom sexp)
@@ -75,8 +112,18 @@ This is the default Lint 'control' rule."
           (cond
             ;; Apply available rules
             ( (symbolp fun)
-              (foreach rule (_\@lint_get_rules fun)
-                (funcall rule sexp messages levels parents)
+              ;; TODO - Find first environment where variable is used
+              ;; TODO - If variable is in an environment, make sure it is callable and check its arguments if they are defined
+              
+              ;; Function is global, check its arguments and its rules
+              (@nif (getd fun)
+                    (@lint_msg sexp messages levels 'WARNING 'NOT_CALLABLE (@str "Function {fun} is not callable"))
+                (unless (errset (check_arguments sexp messages levels (get_arguments fun)) t)
+                  (@lint_msg sexp messages levels 'WARNING 'ARGS_CHECK_FAIL (@str "Error when checking arguments {errset.errset}"))
+                  )
+                (foreach rule (lint_get_rules fun)
+                  (funcall rule sexp messages levels parents envs scheme)
+                  )
                 ))
             ;; TODO - Lists might be callable
             
@@ -86,12 +133,106 @@ This is the default Lint 'control' rule."
         );t
       ));cond ;fun
 
+  (@fun check_arguments
+    ( ( sexp      ?type list                    )
+      ( messages  ?type list                    )
+      ( levels    ?type list                    )
+      ( arguments ?type ( list table list )|nil )
+      )
+    ?doc "Check if SEXP respects ARGLIST."
+    (@nif arguments
+      (@lint_msg sexp messages levels 'INFO 'UNSUPPORTED (@str "`{(car sexp)}` arguments check is not supported, see (@arglist `{(car sexp)}`)"))
+      (destructuringBind ( positional_args key_args rest_arg ) arguments
+        (let ( ( provided_args      (copy (cdr sexp)) )
+               ( remaining_args     nil               )
+               ( missing_args_count 0                 )
+               )
+          ;; Check positional arguments
+          (foreach positional_arg positional_args
+            (cond
+              ;; Some provided arguments remain, pop one
+              ( provided_args
+                (let ( ( arg (pop provided_args) )
+                       )
+                  (and
+                    (symbolp arg)
+                    (eq ? (getchar arg 1))
+                    (neq 'quote (car sexp))
+                    (@lint_msg sexp messages levels 'WARNING 'POSITIONAL_KEY_ARG (@str "`{(car sexp)}` argument {arg} is treated as positional, move or quote it for disambiguation"))
+                    )
+                  ))
+              ;; Missing argument, check if argument is required or optional
+              ( (memq ?def positional_arg->?) )
+              ;; Missing positional argument, report it
+              ( t missing_args_count++ )
+              ));cond ;foreach
+          (when (plusp missing_args_count)
+            (@lint_msg sexp messages levels 'ERROR 'MISSING_ARG (@str "`{(car sexp)}` requires {missing_args_count} more positional arguments"))
+            )
+          ;; Check key arguments
+          (while provided_args
+            (let ( ( arg (pop provided_args) )
+                   )
+              (cond
+                ;; It looks like a key argument is provided
+                ( (and (symbolp arg) (equal ? (getchar arg 1)))
+                  (cond
+                    ;; Argument is unexpected
+                    ( (not key_args[arg])
+                      (@lint_msg sexp messages levels 'WARNING 'EXTRA_KEY_ARG (@str "`{(car sexp)}` extra key argument {arg} is provided"))
+                      (push arg remaining_args)
+                      )
+                    ;; Argument is expected and value is provided
+                    ( provided_args (pop provided_args) )
+                    ;; Argument is expected but has no value
+                    ( t (@lint_msg sexp messages levels 'ERROR 'MISSING_KEY_VALUE (@str "`{(car sexp)} key argument {arg} requires a value`")) )
+                    ))
+                ( t (push arg remaining_args) )
+                );cond
+              ));let ;while
+          (when (and remaining_args (not rest_arg))
+            (@lint_msg sexp messages levels 'ERROR 'EXTRA_ARGS (@str "`{(car sexp)}` extra arguments are provided"))
+            )
+          ));let ;dbind
+      ));fun
+
+  (@fun get_arguments
+    ( ( fun ?type symbol )
+      )
+    ?doc "Return advanced arguments associated to FUN.
+Output is positional arguments, key arguments and rest argument."
+    ?out ( list table list )|nil
+    ?memoize t
+    (let ( ( args_list            (@arglist fun)    )
+           ( positional_arguments (tconc nil nil)   )
+           ( key_arguments        (makeTable t nil) )
+           ( rest_argument        ()                )
+           ( kind                 '@pos             )
+           )
+      (unless (equal '( ( ___ ) ) args_list)
+        (foreach argument args_list
+          (@caseq (type argument)
+            ( symbol (setq kind argument) )
+            ( list
+              (@caseq kind
+                ( @pos  (tconc positional_arguments          argument) )
+                ( @key  (setf key_arguments[(concat ? (car argument))] argument) )
+                ( @rest
+                  (assert (not rest_argument) "Function can have only one @rest argument.")
+                  (setq rest_argument argument)
+                  )
+                ));caseq ;list
+            ));caseq ;foreach
+        (list (cdar positional_arguments) key_arguments rest_argument)
+        );if
+      ));let ;fun
+
   );closure
 
 
 (defun @lint_msg ( sexp messages levels type name msg )
   (tconc messages (list type name levels msg sexp))
-  )
+  nil)
 
 ;; -------------------------------------------------------
 ;; Lint waiver
@@ -127,13 +268,13 @@ This is the default Lint 'control' rule."
   ?functions '( if )
   ?rule_fun
   (lambda ( sexp messages levels @rest _ )
-    ;; Extra arguments
-    (when (nthcdr 4 sexp)
-      (@lint_msg sexp messages levels 'ERROR 'IF_EXTRA_ARGS (@str "`if` only takes three arguments, those are extra: {(nthcdr 4 sexp)}"))
-      )
     ;; (if <cond> nil ...) can be replaced by `unless`
     (unless (nth 2 sexp)
       (@lint_msg sexp messages levels 'INFO 'IF_NIL (@str "(if <cond> nil ...) can be replaced by (unless <cond> ...)"))
+      )
+    ;; (if <cond> <then>) can be replaced by `when`
+    (unless (nthcdr 3 sexp)
+      (@lint_msg sexp messages levels 'INFO 'IF_THEN (@str "(if <cond> <then>) can be replaced by (when <cond> <then>)"))
       )
     ))
 
@@ -174,10 +315,10 @@ This is the default Lint 'control' rule."
   ?functions '( case caseq @case @caseq )
   ?control t
   ?rule_fun
-  (lambda ( sexp messages levels parents )
+  (lambda ( sexp messages levels parents envs scheme )
     (cond
       ;; Check minumun number of arguments
-      ( (not (cddr sexp)) (@lint_msg sexp messages levels 'ERROR 'CASE_MISSING_ARGS (@str "{(car sexp)} requires at least two arguments.")) )
+      ( (not (cddr sexp)) (@lint_msg sexp messages levels 'ERROR 'CASE_MISSING_ARGS (@str "`{(car sexp)}` requires at least two arguments.")) )
       ( t
         (destructuringBind ( fun _val @rest cases ) sexp
           (let ( ( case_sexp_pos 2 )
@@ -186,21 +327,90 @@ This is the default Lint 'control' rule."
             (foreach case cases
               (cond
                 ( (not (listp case))
-                  (@lint_msg sexp messages levels 'ERROR 'CASE_SYNTAX (@str "{fun} argument should be a list: {case}"))
+                  (@lint_msg sexp messages levels 'ERROR 'CASE_SYNTAX (@str "`{fun}` argument should be a list: {case}"))
                   )
                 ;; Parse result S-expressions
                 ( t
                   (let ( ( sexp_pos 1 )
                          )
                     (foreach sub_sexp (cdr case)
-                      (_\@lint_sexp sub_sexp messages (constar sexp_pos++ case_sexp_pos++ levels) (constar case sexp parents))
+                      (_\@lint_sexp sub_sexp messages
+                        (constar sexp_pos++ case_sexp_pos++ levels  )
+                        (constar case       sexp             parents)
+                        envs scheme
+                        )
                       ));foreach ;let
                   ) ;t
                 ));cond ;foreach case
-            );let
-          ));dbind ;t
-      ));cond ;lambda
-  )
+            ));let ;dbind
+        ));t ;cond 
+    ))
+
+;; -------------------------------------------------------
+;; let
+;; -------------------------------------------------------
+
+(@lint_rule
+  ?functions '( let )
+  ?control t
+  ?rule_fun
+  (lambda ( sexp messages levels parents envs scheme )
+    (cond
+      ;; No arguments, this is already reported when checking arguments 
+      ( (not (cdr sexp)) )
+      ( t
+        (destructuringBind ( fun defs @rest body ) sexp
+          (let ( ( env      (makeTable t nil) )
+                 ( sexp_pos 2                 )
+                 )
+            (@nif (listp defs)
+                  (@lint_msg sexp messages levels 'ERROR 'LET_SYNTAX (@str "`{fun}` first argument should be a list: {defs}"))
+              ;; Define each variable in environment
+              (let ( ( def_pos -1 )
+                     var
+                     )
+                (foreach def defs
+                  def_pos++
+                  ;; Make sure variable definition is valid
+                  (setq var nil)
+                  (caseq (type def)
+                    ( symbol (setq var def) )
+                    ( list
+                      (@nif (and (symbolp (car def)) (cdr def) (not (cddr def)))
+                            ;; Definition is wrong, report it
+                            (@lint_msg sexp messages (constar def_pos 1 levels) 'ERROR 'LET_DEF_SYNTAX (@str "`{fun}` binding must be a symbol or symbol-value pair: {def}"))
+                        (setq var (car def))
+                        ;; Check variable definition
+                        (_\@lint_sexp (cadr def) messages
+                          (constar 1   def_pos 1    levels )
+                          (constar def defs    sexp parents)
+                          envs scheme
+                          )
+                        ));nif ;list
+                    (t
+                      ;; Variable definition should be a symbol or a symbol-value pair
+                      (@lint_msg sexp messages (constar def_pos 1 levels) 'ERROR 'LET_DEF_SYNTAX (@str "`{fun}` binding must be a symbol or symbol-value pair: {def}"))
+                      ));t ;caseq
+                    ;; Add variable to env
+                    (when var (setf env[var] (list nil 'status 'unused)))
+                    ));foreach ;let
+              (push env envs)
+              ;; Check body
+              (foreach sub_sexp body
+                (_\@lint_sexp sub_sexp messages (cons sexp_pos++ levels) (cons sexp parents) envs scheme)
+                )
+              ;; Check unused variables
+              (foreach name env[?]
+                (@caseq env[name]->status
+                  ( used     nil )
+                  ( assigned (@lint_msg sexp messages levels 'WARNING 'LET_ASSIGNED_ONLY (@str "`{fun}` variable {name} is assigned only")) )
+                  ( t        (@lint_msg sexp messages levels 'WARNING 'LET_UNUSED        (@str "`{fun}` variable {name} is unused"       )) )
+                  ));caseq ;foreach
+              );nif
+            ));let ;dbind
+        ));t ;cond
+    ))
+
 
 ;; =======================================================
 ;; Apply rules
@@ -217,6 +427,7 @@ This is the default Lint 'control' rule."
 All report messages are printed to PORT."
     ?out t|nil
     ?global t
+    (assert files "@lint - ?files is nil")
     ;; -------------------------------------------------------
     ;; Parse S-expressions of each file
     ;; -------------------------------------------------------
@@ -231,6 +442,7 @@ All report messages are printed to PORT."
                  ( pos                 0               )
                  ( line                1               )
                  ( results_by_top_sexp (tconc nil nil) )
+                 ( scheme              (pcreMatchp "\\.(ils|scm)$" file))
                  )
             (@while (lineread in_port)
               ?var sexps
@@ -246,7 +458,7 @@ All report messages are printed to PORT."
                          ( sexp_pos 0                                                                        )
                          )
                     (foreach sexp sexps
-                      (_\@lint_sexp sexp messages (list sexp_pos++) sexps)
+                      (_\@lint_sexp sexp messages (list sexp_pos++) sexps nil scheme)
                       );foreach
                     ;; Find actual lines where messages occured
                     (setq messages
